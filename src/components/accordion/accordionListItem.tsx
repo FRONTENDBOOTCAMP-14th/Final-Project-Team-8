b5b209c2d4860f2a7a7bcdade32c4666bb849132
cit1566 <cit1566@gmail.com>
2025-09-30 09:39:56 +0900

    [feat] 아코디언 컴포넌트 분리 - 확장 가로 중단선 추가 - 더미 데이터를 이용한 리스트 렌더링

---
+++ src/components/accordion/accordionListItem.tsx
@@ -0,0 +1,32 @@
+import { CalendarIcon } from 'lucide-react'
+
+// 리스트 아이템 Props 타입
+interface AccordionListItemProps {
+  title: string
+  date: string | number
+}
+
+/**
+ * AccordionListItem 컴포넌트
+ * 년도별 백신 기록 한 줄 렌더링
+ */
+export function AccordionListItem({ title, date }: AccordionListItemProps) {
+  return (
+    <section className="m-5 flex items-center rounded-xl border border-gray-300 pt-[23px] pr-4 pb-[23px] pl-4">
+      {/* 백신 이름 */}
+      <span
+        aria-label={title}
+        className="grow text-base font-bold text-gray-800"
+      >
+        {title}
+      </span>
+
+      {/* 구분선 */}
+      <div className="relative mr-3 ml-3 flex items-center before:absolute before:left-0 before:h-4 before:w-px before:bg-gray-300"></div>
+
+      {/* 날짜 아이콘 및 표시 */}
+      <CalendarIcon width={22} height={22} className="text-gray-400" />
+      <span className="ml-2 font-bold text-gray-500">{date}</span>
+    </section>
+  )
+}
