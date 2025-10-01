cc1a7366ada3dd1693b1119f95f20818e77d0635
cit1566 <cit1566@gmail.com>
2025-10-01 13:42:57 +0900

    [fix] page.HomePage 컴포넌트 원상복구

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
-import Accordion from '../components/accordion/accordion'
+import { LearnSection } from '@/components'
+import RandomCountUp from '@/demo'
 
 export default function HomePage() {
   return (
-    <section className="overflow-hidden">
-      <Accordion type="vaccination" title="예방접종"></Accordion>
-      <Accordion type="anthelmintic" title="구충 치료"></Accordion>
-      <Accordion type="medical" title="의료 처치"></Accordion>
-      <Accordion type="other-treatments" title="기타 치료"></Accordion>
-      <Accordion type="food-journal" title="식단 일지"></Accordion>
-      <Accordion type="walk" title="산책"></Accordion>
-      <Accordion type="other-journals" title="기타 활동 일지"></Accordion>
+    <section>
+      <LearnSection title="랜덤 카운트 업">
+        <RandomCountUp />
+      </LearnSection>
     </section>
   )
 }
