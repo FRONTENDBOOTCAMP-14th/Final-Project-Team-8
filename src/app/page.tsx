f9d9fed2204194023654d7af9c22fdfbaa092db9
cit1566 <cit1566@gmail.com>
2025-10-02 13:13:37 +0900

    [feat] modal 컴포넌트 제잠

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,45 @@
-import { LearnSection } from '@/components'
-import RandomCountUp from '@/demo'
+import ModalBox from '../components/modal/ModalBox'
 
 export default function HomePage() {
   return (
-    <section>
-      <LearnSection title="랜덤 카운트 업">
-        <RandomCountUp />
-      </LearnSection>
-    </section>
+    <div>
+      <section>
+        {/* <LearnSection title="랜덤 카운트 업">
+          <RandomCountUp />
+        </LearnSection> */}
+        <h1 className="m-5 text-center text-3xl font-bold">
+          <span className="rounded-2xl bg-indigo-200 pt-2 pr-3 pb-2 pl-3 text-indigo-600">
+            모달 제작
+          </span>
+        </h1>
+        <div>
+          <ModalBox title="다이얼로그 제목이지요~">
+            <div>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Delectus, doloremque. Eligendi dolorum obcaecati impedit sequi.
+                Ea quia doloremque praesentium est iure? Corrupti nostrum libero
+                eaque, aperiam debitis reiciendis facilis neque!
+              </p>
+              <input
+                type="text"
+                className="rounded-md border-2 border-indigo-300 p-2 focus:border-indigo-500 focus:outline-none"
+              />
+              <button
+                type="button"
+                className="m-3 rounded-lg bg-indigo-300 p-2 text-xl text-white transition hover:bg-indigo-500 active:scale-[0.95]"
+              >
+                aaa
+              </button>
+            </div>
+          </ModalBox>
+
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </section>
+      <section id="modal-dialog-portal" aria-live="assertive"></section>
+    </div>
   )
 }
---
+++ src/components/modal/Modal.tsx
@@ -0,0 +1,121 @@
+'use client'
+
+import { X } from 'lucide-react'
+import {
+  PropsWithChildren,
+  useCallback,
+  useEffect,
+  useId,
+  useRef,
+  useState,
+} from 'react'
+import { createPortal } from 'react-dom'
+import Button from '../ui/button/Button'
+
+type Props = PropsWithChildren<{
+  open?: boolean
+  onClose?: () => void
+  title?: string
+  describe?: string
+}>
+
+export default function Modal({
+  open = false,
+  onClose,
+  title,
+  describe,
+  children,
+}: Props) {
+  // createPortal에 전달되는 루트 요소
+  const [portalEl, setPortalEl] = useState<HTMLElement | null>(null)
+
+  // 수정 상태 변경 저장
+  const [isModify, setModify] = useState<boolean>(false)
+
+  //
+  const opennerRef = useRef<HTMLElement>(null)
+  const dialogRef = useRef<HTMLDialogElement>(null)
+  const dialogId = useId()
+  const titleId = `${dialogId}-title`
+  const describeId = `${dialogId}-describe`
+
+  const close = useCallback(() => {
+    opennerRef.current?.focus()
+    onClose?.()
+  }, [onClose])
+
+  useEffect(() => {
+    const dialog = dialogRef.current
+    if (!dialog) return
+
+    if (open) {
+      dialog.showModal()
+    } else {
+      dialog.close()
+    }
+
+    // 모달 다이얼로그의 오버레이 영역 클릭 시 닫기 기능 구현
+    const handleCloseByBackdrop = (e: globalThis.MouseEvent) => {
+      if (e.target === dialog) onClose?.()
+    }
+    dialog.addEventListener('click', handleCloseByBackdrop)
+
+    return () => {
+      dialog.removeEventListener('click', handleCloseByBackdrop)
+    }
+  }, [open, onClose])
+
+  useEffect(() => {
+    if (typeof document !== 'undefined') {
+      setPortalEl(document.getElementById('modal-dialog-portal'))
+    }
+  }, [])
+
+  if (!open || !portalEl) return null
+
+  return createPortal(
+    <dialog
+      ref={dialogRef}
+      aria-modal="true"
+      aria-labelledby={titleId}
+      aria-describedby={describe ? describeId : undefined}
+      className="m-auto rounded-3xl border-0 bg-white shadow-xl backdrop:backdrop-blur-[3px]"
+    >
+      <div className="felx flex min-h-55 w-170 flex-col p-[30px]">
+        <div className="flex">
+          {!isModify ? (
+            <h1
+              id={titleId}
+              className="grow text-[28px] font-bold text-gray-800"
+            >
+              {title ?? '다이얼로그 제목'}
+            </h1>
+          ) : (
+            <input type="text" defaultValue={title} />
+          )}
+          <button
+            type="button"
+            aria-label="다이얼로그 닫기"
+            title="다이얼로그 닫기"
+            onClick={close}
+            className="cursor-pointer rounded-full text-gray-600 transition active:scale-[0.95]"
+          >
+            <X width={20} height={20} />
+          </button>
+        </div>
+        {describe && <div id={describeId}>{describe}</div>}
+        {children}
+
+        <Button
+          className="!m-0 !p-0"
+          onClick={() => {
+            setModify(prev => !prev)
+          }}
+        >
+          수정
+        </Button>
+      </div>
+    </dialog>,
+    portalEl
+  )
+}
