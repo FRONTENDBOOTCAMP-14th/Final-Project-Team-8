7cf8114afbcadcafa32c096a39ed8b150bbc5a0e
cit1566 <cit1566@gmail.com>
2025-10-10 14:58:17 +0900

    [fix] 메인 페이지 복구

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,12 @@
-import Accordion from '../components/accordion/accordion'
+import { LearnSection } from '../components'
+import RandomCountUp from '../demo'
 
 export default function HomePage() {
   return (
     <section>
-      {/* <LearnSection title="랜덤 카운트 업">
+      <LearnSection title="랜덤 카운트 업">
         <RandomCountUp />
-      </LearnSection> */}
-      <Accordion type="vaccines" title="예방접종"></Accordion>
-      <Accordion type="antiparasitic" title="구충 치료"></Accordion>
-      <Accordion type="medical treatment" title="의료 처치"></Accordion>
-      <Accordion type="other treatments" title="기타 치료"></Accordion>
-      <Accordion type="diet" title="식단 일지"></Accordion>
-      <Accordion type="other activities" title="기타 활동"></Accordion>
-      <Accordion type="walks" title="산책"></Accordion>
+      </LearnSection>
     </section>
   )
 }
