7fdcba96c94ad35da01405e558c3f0153ee10132
cit1566 <cit1566@gmail.com>
2025-10-10 14:45:59 +0900

    [feat] 모달 각 타입별 컴포넌트 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,12 @@
-import ModalBox, { ModalBoxInput } from '../components/modal/ModalBox'
+import { LearnSection } from '../components'
+import RandomCountUp from '../demo'
 
 export default function HomePage() {
   return (
-    <div>
-      <section>
-        {/* <LearnSection title="랜덤 카운트 업">
-          <RandomCountUp />
-        </LearnSection> */}
-        <ModalBox title="모달" modalDetail={false}>
-          <div>
-            <h1>기본 타입이 없으면 아무것도 나오지 않아요!</h1>
-            <div>
-              <label htmlFor="input-A">아이디</label>
-              <input type="text" id="input-A" />
-            </div>
-          </div>
-        </ModalBox>
-        <ModalBoxInput></ModalBoxInput>
-      </section>
-
-      <section id="modal-dialog-portal"></section>
-    </div>
+    <section>
+      <LearnSection title="랜덤 카운트 업">
+        <RandomCountUp />
+      </LearnSection>
+    </section>
   )
 }
