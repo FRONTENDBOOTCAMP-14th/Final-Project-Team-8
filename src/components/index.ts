627c80b378f83d9a75159a058cdd47bb2086c1ee
WUHYERI <woohree0125@gmail.com>
2025-10-10 17:36:22 +0900

    [chore] 불필요한 파일 삭제 및 index.ts 수정

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,8 +3,11 @@ export { default as Button } from './ui/button/Button'
 export { default as CardButton } from './ui/button/CardButton'
 export { default as ImgCardButton } from './ui/button/ImgCardButton'
 
-export { default as DynamicImage } from './ui/dynamic-image'
+// 사이드바
+export { default as Sidebar } from './ui/sidebar/Sidebar'
+
+// 프로필카드
+export { default as PetProfileCard } from './pet-profile/PetProfileCard'
+export { default as UserProfileCard } from './user-profile/UserProfileCard'
 
 export { default as LearnSection } from './learn/section'
-
-export { default as Sidebar } from './ui/sidebar/Sidebar'
