081d021d07630aaf162fdce5e5a2a63599491d8a
cit1566 <cit1566@gmail.com>
2025-10-13 16:01:12 +0900

    [move] utill 폴더로 이동

--- src/components/accordion/accordionFun.ts
+++ src/components/accordion/accordionFun.ts
@@ -47,12 +47,3 @@ export const selectTypeButtonTitle = (type: AccordionProps['type']): string => {
       return 'null'
   }
 }
-
-// 아코디언 날짜 0000-00-00 형식 변환 함수
-export function toISODate(display: string | null) {
-  if (display === null) return
-  const isoLike = /^\d{4}-\d{2}-\d{2}/.test(display)
-  if (isoLike) return display
-  const d = new Date(display)
-  return isNaN(d.getTime()) ? display : d.toISOString().slice(0, 10)
-}
