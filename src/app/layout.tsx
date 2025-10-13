4306ba983142caf59756cc91d368be368370e42d
cit1566 <cit1566@gmail.com>
2025-10-13 11:06:43 +0900

    [fix] main 컴포넌트 Providers 묶음 - QueryClient를 반환 - QueryClient : React Query의 캐시, 요청, 상태, 에러, 로딩등을 관리하는 핵심 객체

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import '@/styles/main.css'
 import { Metadata } from 'next'
 import type { PropsWithChildren } from 'react'
 import { Toaster } from 'sonner'
+import Providers from './providers'
 
 export const metadata: Metadata = {
   title: 'PAW BUDDY',
@@ -12,8 +13,10 @@ export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="ko-KR">
       <body>
-        <Sidebar></Sidebar>
-        <main>{children}</main>
+        <Providers>
+          <Sidebar></Sidebar>
+          <main>{children}</main>
+        </Providers>
         <Toaster position="top-center" richColors />
       </body>
     </html>
