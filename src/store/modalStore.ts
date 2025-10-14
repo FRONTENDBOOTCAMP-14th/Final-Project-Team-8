2cc5afc986672340dd0a93e8086bf93a033c1ce1
cit1566 <cit1566@gmail.com>
2025-10-14 15:01:09 +0900

    [chore] usePostPetData 주석

--- src/store/modalStore.ts
+++ src/store/modalStore.ts
@@ -12,11 +12,19 @@ type Action = {
 }
 
 export const useModal = create<State & Action>(set => ({
+  // 모달 On/Off
   isToggle: false,
+
+  // 모달의 수정 상태 On/Off
   isModify: true,
+
+  // 위 State 전달시 => True 변환
   onToState: state => {
     set({ [state]: true })
   },
+  // 위 State 전달시 => False 변환
   offToState: () => set({ isToggle: false }),
+
+  // 위 State 전달시 => boolean 역변환
   toggleToState: () => set(state => ({ isToggle: !state.isToggle })),
 }))
