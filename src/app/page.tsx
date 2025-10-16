f939d5a9e8fe0a25ee529281329178a5b2a8baca
cit1566 <cit1566@gmail.com>
2025-10-16 16:45:28 +0900

    [fix] page.tsx 원상복구

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,7 @@
-import { Sidebar } from '../components'
-import Accordion from '../components/accordion/accordion'
-
 export default function HomePage() {
   return (
     <div>
-      <section className="m-0 flex w-screen p-0">
-        <Sidebar></Sidebar>
-
-        <div className="w-full">
-          <Accordion type="antiparasitic" title="구충치료" />
-          <Accordion type="diet" title="식단 일지" />
-          <Accordion type="medical treatment" title="의료 처치" />
-          <Accordion type="other activities" title="기타 활동" />
-          <Accordion type="other treatments" title="기타 치료" />
-          <Accordion type="vaccines" title="예방접종" />
-          <Accordion type="walks" title="산책" />
-        </div>
-      </section>
+      <section className="m-0 flex w-screen p-0"></section>
 
       <section id="modal-dialog-portal"></section>
     </div>
