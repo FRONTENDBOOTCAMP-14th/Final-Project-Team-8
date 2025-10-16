b6c02e258e9230c26b314f05fa8f79e47bb8ee86
cit1566 <cit1566@gmail.com>
2025-10-16 23:27:11 +0900

    [fix] 라이브서버 오류 수정 - QueryClientProvider는 client 상태에서 작동하는 - 때문에 Provider는 Client 상태여야 한다.

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,9 +1,10 @@
+'use client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import type { ReactNode } from 'react'
+import { useState, type ReactNode } from 'react'
 // (선택) Devtools: import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-const queryClient = new QueryClient()
 export default function Providers({ children }: { children: ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient())
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   )
