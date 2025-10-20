35a2bad96ed48d780948e3f96ba91811b523007d
cit1566 <cit1566@gmail.com>
2025-10-20 16:13:55 +0900

    [fix] 메인 페이지 원상복구

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,7 @@
-import { Sidebar } from '../components'
-import Accordion from '../components/accordion/accordion'
-
 export default function HomePage() {
   return (
     <div>
-      <section>
-        <Sidebar></Sidebar>
-        <Accordion type="antiparasitic" title="구충 치료"></Accordion>
-        <Accordion type="diet" title="식단 일지"></Accordion>
-        <Accordion type="medical treatment" title="의료 처치"></Accordion>
-        <Accordion type="other activities" title="기타 활동"></Accordion>
-        <Accordion type="other treatments" title="기타 치료"></Accordion>
-        <Accordion type="vaccines" title="예방접종"></Accordion>
-        <Accordion type="walks" title="산책"></Accordion>
-      </section>
+      <section></section>
 
       <section id="modal-dialog-portal"></section>
     </div>
