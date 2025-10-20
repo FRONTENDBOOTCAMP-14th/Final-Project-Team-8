47b1aa5b0fa57f26659e0d1238bdcb25463cf337
cit1566 <cit1566@gmail.com>
2025-10-20 15:21:27 +0900

    [fix] 변경 파일 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import { Sidebar } from '../components'
+import Accordion from '../components/accordion/accordion'
+
 export default function HomePage() {
   return (
     <div>
-      <section></section>
+      <section>
+        <Sidebar></Sidebar>
+        <Accordion type="antiparasitic" title="구충 치료"></Accordion>
+        <Accordion type="diet" title="식단 일지"></Accordion>
+        <Accordion type="medical treatment" title="의료 처치"></Accordion>
+        <Accordion type="other activities" title="기타 활동"></Accordion>
+        <Accordion type="other treatments" title="기타 치료"></Accordion>
+        <Accordion type="vaccines" title="예방접종"></Accordion>
+        <Accordion type="walks" title="산책"></Accordion>
+      </section>
 
       <section id="modal-dialog-portal"></section>
     </div>
