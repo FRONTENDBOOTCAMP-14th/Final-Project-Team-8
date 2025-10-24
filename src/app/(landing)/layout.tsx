f291eb41fb255e24fd7de1c5fa053f21e3ea0418
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-10-24 11:49:52 +0900

    [feat] 랜딩, 로그인 및 회원가입 페이지에서 좌측 상단 로고 클릭 시 대시보드로 이동

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,12 +1,17 @@
+import Link from 'next/link'
 import type { PropsWithChildren } from 'react'
 
 export default function LandingPageLayout({ children }: PropsWithChildren) {
   return (
     <div className="relative flex h-screen w-full flex-col gap-8 bg-gradient-to-b from-[#FFF5E1] to-[#FFE0B2] p-10">
       {/* 상단 PAW BUDDY LOGO */}
-      <div className="absolute">
-        <img src="/assets/logo/Logo-Paw-Buddy-col.svg" alt="logo" />
-      </div>
+      <Link href="/dashboard" className="absolute z-50 cursor-pointer">
+        <img
+          src="/assets/logo/Logo-Paw-Buddy-col.svg"
+          alt="logo"
+          className="transition-opacity hover:opacity-80"
+        />
+      </Link>
       {/* 좌우 레이아웃 */}
       <div className="flex h-full w-full flex-col-reverse gap-5 md:flex-row">
         {/* 왼쪽 이미지 */}
