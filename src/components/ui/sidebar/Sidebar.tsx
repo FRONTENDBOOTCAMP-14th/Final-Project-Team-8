9a11abe31a78176573f312f37e054b0064783f0a
cit1566 <cit1566@gmail.com>
2025-10-28 16:27:05 +0900

    [fix] lint Error

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Divide, PawPrint } from 'lucide-react'
+import { PawPrint } from 'lucide-react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import type { ReactNode } from 'react'
